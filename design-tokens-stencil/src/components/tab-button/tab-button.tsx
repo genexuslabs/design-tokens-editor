561703dbd79233bc367cce0a95893c4ce6c00d2e
bsastre <bsastre@genexus.com>
2019-11-05 12:56:01 -0300

    tab-button changes

--- design-tokens-stencil/src/components/tab-button/tab-button.tsx
+++ design-tokens-stencil/src/components/tab-button/tab-button.tsx
@@ -1,4 +1,5 @@
 import { Component, Prop, h } from "@stencil/core";
+import { Listen } from "@stencil/core";
 
 @Component({
   tag: "dt-tab-button",
@@ -8,14 +9,25 @@ import { Component, Prop, h } from "@stencil/core";
 export class TabButton {
   // Indicate that name should be a public property on the component
   @Prop() tab: string;
-  @Prop() disabled: boolean = false;
+  @Prop() isSelected: boolean = false;
+
+  tabButtonClicked() {
+    this.isSelected = true;
+  }
 
   render() {
     return (
       <li class="tab-item">
-        <button class="tab-button" disabled={this.disabled === true}>
+        <button
+          class={{
+            "tab-button": true,
+            "tab-button--selected": this.isSelected === true
+          }}
+          onClick={this.tabButtonClicked.bind(this)}
+        >
           {this.tab}
         </button>
+        <slot></slot>
       </li>
     );
   }
